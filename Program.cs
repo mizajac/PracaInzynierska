63f3cca87f281ab662cc5dfbd96494e71a5b3e7b
mizajac <98713446+mizajac@users.noreply.github.com>
2022-03-21 10:57:49 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using PracaInz.Data;
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<PracaInzContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("PracaInzContext")));
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
